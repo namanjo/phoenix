ca83dcaf7926f44fc478ccc3e106168965f43101
Naman Joshi <naman14joshi@gmail.com>
2022-11-30 10:37:41 +0530

    First Commit

---
+++ mypay/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
+
+
+
+# Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=200)
+    acc_no = models.CharField(max_length=200, null=True)
+    qr_code = models.ImageField(upload_to='qr_codes', blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        qrcode_img = qrcode.make([self.name, self.acc_no])
+        canvas = Image.new('RGB', (290, 290), 'white')
+        canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+        canvas.paste(qrcode_img)
+        fname = f'qr_code-{self.name}.png'
+        buffer = BytesIO()
+        canvas.save(buffer,'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
